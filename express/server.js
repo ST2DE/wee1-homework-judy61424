1dd19b6b60837bf95a5b93ee1a53d7f8ab0a4749
judy61424 <34883820+judy61424@users.noreply.github.com>
2018-04-25 22:03:45 +0800

    i

--- express/server.js
+++ express/server.js
@@ -1,13 +1,18 @@
-var express = require('express');
-var app = express();
+const express = require('express');
+const app = express(); 
 
-var bodyParser = require('body-parser');
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(__dirname + '/public')); 
 
-// app.set('view engine','ejs');
-express.static('./views')
-app.listen(3000, function() {
-  console.log('server start!')
+app.set('view engine', 'ejs');
+app.get('/about_me', function (req, res) {
+    res.render('index', { name: req.query.name });
 });
 
-var routes = require('./routes')(app);
+app.get('/', function (req, res) {
+    res.send("Who are <b>YOU!</b>");
+});
+
+
+app.listen(3000, function () {
+    console.log('The server has been run on http://localhost:3000/');
+});
